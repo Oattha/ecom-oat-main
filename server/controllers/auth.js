09f821468c8121136cba8177f5c6d6043a8a6830
Oattha <facup877@gmail.com>
2025-02-25 16:33:47 +0700

    SA_1

---
+++ server/controllers/auth.js
@@ -0,0 +1,91 @@
+const prisma = require('../config/prisma');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    try {
+        const { email, password, role } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: 'กรุณากรอกข้อมูลให้ครบ' });
+        }
+
+        const user = await prisma.user.findFirst({
+            where: { email: email }
+        });
+
+        if (user) {
+            return res.status(400).json({ message: "Email already exists!" });
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        await prisma.user.create({
+            data: {
+                email: email,
+                password: hashPassword,
+                role: role || "user", // ✅ Default เป็น user
+                enabled: true // ✅ ให้ user ใช้งานได้เลย
+            }
+        });
+
+        res.send('Register Success');
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await prisma.user.findFirst({
+            where: { email: email }
+        });
+
+        if (!user) {
+            return res.status(400).json({ message: 'User Not found' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Password Invalid!' });
+        }
+
+        const payload = {
+            id: user.id,
+            email: user.email,
+            role: user.role
+        };
+
+        const SECRET_KEY = process.env.SECRET || "defaultsecret";
+
+        jwt.sign(payload, SECRET_KEY, { expiresIn: '1d' }, (err, token) => {
+            if (err) {
+                return res.status(500).json({ message: "Server Error" });
+            }
+            res.json({ payload, token });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+exports.currentUser = async (req, res) => {
+    try {
+        const user = await prisma.user.findFirst({
+            where: { email: req.user.email },
+            select: {
+                id: true,
+                email: true,
+                role: true
+            }
+        });
+        res.json({ user });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+}
